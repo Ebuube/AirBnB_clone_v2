8e9cbaac94f6f0b945569f619326867386c087ab
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-10 17:05:53 +0000

    Full deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Full Deployment
+"""
+from fabric.api import *
+from os.path import isfile
+from datetime import datetime
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+
+env.hosts = ['18.207.2.78', '54.162.43.21']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+# Compress depolyment package
+recent_archive = do_pack()
+
+
+def deploy(archive=recent_archive):
+    """
+    Create and distribute an archive to my web servers
+    """
+
+    # Compress archive
+    try:
+        if not isfile(archive):
+            return False
+    except Exception:
+        return False
+
+    # Deploy archive
+    res = do_deploy(archive)
+    return res
