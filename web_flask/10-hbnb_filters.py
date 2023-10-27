f85c470424246d87eb7ce484280e0b92c9d75f48
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-27 16:43:57 +0000

    removed debug mode

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -35,5 +35,5 @@ def set_filters():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
